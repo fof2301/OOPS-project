2cd86f6fe85f4564a888bb421ecfbe855f7ab39b
Shreyansh Kushwaha <114877979+shreyanshakafof@users.noreply.github.com>
2023-05-16 00:01:01 +0530

    new file:   .DS_Store 	new file:   221010220_ishita_whistle.stl 	modified:   main.py 	new file:   output_image.png 	new file:   textfile.txt

---
+++ main.py
@@ -0,0 +1,19 @@
+import binascii
+import text_to_image
+#file to hex
+filename = "221010220_ishita_whistle.stl"
+with open(filename, "rb") as f:
+  content=f.read()
+  hex_Data=binascii.hexlify(content)
+print(hex_Data)  
+with open("textfile.txt", 'wb') as f:
+    f.write(hex_Data)
+#encoded image generator
+encoded_image_path = text_to_image.encode_file("textfile.txt", "output_image.png")    
+#decoder 
+decoded_file_path = text_to_image.decode_to_file("output_image.png","test.txt")
+#hex to file
+import binascii
+with open("test.txt") as f, open("out", 'wb') as out:
+    for line in f:
+        out.write(binascii.unhexlify(''.join(line.split())))
